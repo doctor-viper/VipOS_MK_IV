37fc302fef21a104fa09704042fe064d322a45e0
doctor-viper <doctor.dark.viper@gmail.com>
2022-12-01 20:22:06 -0500

    Template updates, initial overlays / stylesheets

--- app.js
+++ app.js
@@ -1,83 +1,56 @@
+const express = require('express');
+// const path = require('path');
 
-// import { RefreshingAuthProvider } from '@twurple/auth';
-// import { ChatClient } from '@twurple/chat';
-// import { promises as fs } from 'fs';
+const app = express();
 
-// import ejs from 'ejs';
-import express from 'express';
-import path from 'path';
-import {fileURLToPath} from 'url';
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const app = express()
 
-// setup static and middleware
+/**
+ * Setup static directory
+ *  and rendering engine
+ * 
+ */
 app.use(express.static('./public'))
-app.set("view enginge","ejs")
+app.set("view engine","ejs")
 
+
+/**
+ * Home Page
+ * 
+ */
 app.get('/', (req, res) => {
   res.send('VipOS MARK IV')
   //res.sendFile(path.resolve(__dirname, './templates/header.html'))
 })
 
-app.get('/animated-bg', (req, res) => {
-    res.render('animated_bg.ejs')
-  })
 
+/**
+ * Overlays
+ * 
+ */
+app.get('/overlay/cam-bg', (req, res) => {
+  res.render('cam_bg.ejs')
+})
+
+app.get('/overlay/info-bar', (req, res) => {
+  res.render('info_bar.ejs')
+})
+
+/**
+ * 404 / All
+ * 
+ */
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
 })
 
+
+/**
+ * Listen on port
+ * 
+ */
 app.listen(5000, () => {
   console.log('server is listening on port 5000....')
 })
-
-/**
- * 
- *  V I P E R
- * 
- *  Read this article for listening to Twitch Events - MK 4 MUTHAFUCKA
- * 
- */
-// https://d-fischer.github.io/twitch-eventsub/docs/basic-usage/listening-to-events.html
-
-// async function main() {
-// 	const clientId = '';
-// 	const clientSecret = '';
-// 	const tokenData = JSON.parse(await fs.readFile('./tokens.json', 'UTF-8'));
-// 	const authProvider = new RefreshingAuthProvider(
-// 		{
-// 			clientId,
-// 			clientSecret,
-// 			onRefresh: async newTokenData => await fs.writeFile('./tokens.json', JSON.stringify(newTokenData, null, 4), 'UTF-8')
-// 		},
-// 		tokenData
-// 	);
-
-// 	const chatClient = new ChatClient({ authProvider, channels: ['dr_viper'] });
-// 	await chatClient.connect();
-
-// 	chatClient.onMessage((channel, user, text) => {
-// 		if (text === '!ping') {
-// 			chatClient.say(channel, 'Pong!');
-// 		} else if (text === '!dice') {
-// 			const diceRoll = Math.floor(Math.random() * 6) + 1;
-// 			chatClient.say(channel, `@${user} rolled a ${diceRoll}`)
-// 		}
-// 	});
-
-// 	chatClient.onSub((channel, user) => {
-// 		chatClient.say(channel, `Thanks to @${user} for subscribing to the channel!`);
-// 	});
-	
-//     chatClient.onResub((channel, user, subInfo) => {
-// 		chatClient.say(channel, `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
-// 	});
-
-// 	chatClient.onSubGift((channel, user, subInfo) => {
-// 		chatClient.say(channel, `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
-// 	});
-// }
-
-// main();
