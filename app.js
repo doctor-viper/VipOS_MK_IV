09cc00273631b5c7c234326b11e51816c0a712a2
doctor-viper <doctor.dark.viper@gmail.com>
2023-01-27 14:08:24 -0500

    New Twurple API and EventSub WS + GOLD VENOM COIN OVERLAY

--- app.js
+++ app.js
@@ -12,59 +12,12 @@ const client = require('./client-info');
 // Twurple Init
 const { RefreshingAuthProvider } = require('@twurple/auth');
 const { ChatClient } = require('@twurple/chat');
+const { ApiClient } = require('@twurple/api');
+const { EventSubWsListener } = require('@twurple/eventsub-ws');
 
 // FileSystem
 const { promises: fs } = require('fs');
 
-
-/**
- *  Connect to Chat
- * 
- */
-async function main() {
-	const clientId = client.id;
-	const clientSecret = client.secret;
-	const tokenData = JSON.parse(await fs.readFile('./tokens.json', 'UTF-8'));
-	const authProvider = new RefreshingAuthProvider(
-		{
-			clientId,
-			clientSecret,
-			onRefresh: async newTokenData => await fs.writeFile('./tokens.json', JSON.stringify(newTokenData, null, 4), 'UTF-8')
-		},
-		tokenData
-	);
-
-	const chatClient = new ChatClient({ authProvider, channels: ['dr_viper'] });
-	await chatClient.connect();
-
-	chatClient.onMessage((channel, user, text) => {
-    
-    const regex = /vipos|vip os|viper os|viperos/ig; 
-    if( text.match(regex) != null && user != 'vipos_mk3' ) {
-      // Detect for VipOS references
-    }
-
-		if (text === '!ping') {
-			chatClient.say(channel, 'Pong!');
-		} else if (text === '!dice') {
-			const diceRoll = Math.floor(Math.random() * 6) + 1;
-			chatClient.say(channel, `@${user} rolled a ${diceRoll}`)
-		}
-
-	});
-
-	chatClient.onSub((channel, user) => {
-		chatClient.say(channel, `Thanks to @${user} for subscribing to the channel!`);
-	});
-	chatClient.onResub((channel, user, subInfo) => {
-		chatClient.say(channel, `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
-	});
-	chatClient.onSubGift((channel, user, subInfo) => {
-		chatClient.say(channel, `Thanks to ${subInfo.gifter} for gifting a subscription to ${user}!`);
-	});
-}
-
-main();
 
 /**
  * Setup static directory
@@ -96,6 +49,10 @@ app.get('/overlay/info-bar', (req, res) => {
   res.render('info_bar.ejs')
 })
 
+app.get('/overlay/venom-coin',(req, res) => {
+  res.render('venom_coin.ejs')
+})
+
 app.get('/overlay/info-bar-side', (req, res) => {
   res.render('info_bar_side.ejs')
 })
@@ -108,12 +65,11 @@ app.get('/overlay/knight-rider', (req, res) => {
   res.render('knight_rider.ejs')
 })
 
-
-app.get('/api/v1/text', (req,res) => {
-  io.emit('text-alert', { message: 'super cool new test' }); // This will emit the event to all connected sockets
+app.post('/api/v1/text', express.json(), (req, res) => {
+  io.emit('text-alert', { message: req.body.msg });
+  console.log(req.body); //TO-DO: remove this when done
   res.sendStatus(200);
 })
-
 
 /**
  * 404 / All
@@ -128,6 +84,6 @@ app.all('*', (req, res) => {
  * Listen on port
  * 
  */
-server.listen(5000, () => {
+server.listen(5000, async () => {
   console.log('server is listening on port 5000....')
 })
